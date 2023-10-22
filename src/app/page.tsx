f6f49b18ff6242c90c7bfe5c77fee45ec47ba8c0
Sagar Rathod <97072541+sagarbrathod1@users.noreply.github.com>
2023-10-21 20:34:49 -0400

    initial home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,38 @@
 import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs";
+import { LogIn } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
-  return <Button>Click Me</Button>;
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
+
+  return (
+    <div className="w-screen min-h-screen bg-gradient-to-r from-pink-100 to-blue-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Talk to your PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+          <div className="flex mt-4">
+            {isAuth && <Button>Go to Chats</Button>}
+          </div>
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>fileupload</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login
+                  <LogIn className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
